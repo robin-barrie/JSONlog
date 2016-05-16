c2afe01d228e8f3ded7fb31a74258a6fe68a0766
robin-barrie <rlbarrie88@gmail.com>
2016-05-15 23:02:36 -0400

    Convert to StringBuilder
    abandon JSONObject and start using StringBuilder

--- src/Test.java
+++ src/Test.java
@@ -3,145 +3,93 @@
 import java.io.FileWriter;
 import java.io.IOException;
  
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
- 
  
 public class Test {
 	
-	public static JSONObject stated;
-	public static JSONObject logFile;
-	public static JSONArray state;
+	public static StringBuilder builder = new StringBuilder();
+	
 	
 	public static void main(String[] args)  {
 		
-		stated = new JSONObject();
-		logFile = new JSONObject();
-		state = new JSONArray();
 	
-		
- 
-		ioinfoJSON();
-		for(int i=1; i<3; i++){
-		 		stateJSON();
-		}
-		printJSON();
-		
+		testIOInfoSB(builder);
+		testStateSB(builder);
+		testFinalStateSB(builder);
+			
 	}
 	
 	
-	//upfate to just print
-		public static void stateJSON() {
-		//JSONArray state = new JSONArray();
-		JSONObject states = new JSONObject();
+	public static void testIOInfoSB(StringBuilder stuff) {
 		
-		JSONObject button3 = new JSONObject();
-		 //button3 = "{\"name\":\"Driver button 3\",\"parent\":\"Driver button 3\",\"value\":\"false\"},";
-		//String buttonString = "\"test";
-		//button3 = JSON.parse(buttonString);
+		builder = stuff;
 		
 		
-		JSONObject button1 = new JSONObject();
-		button1.put("name", "button1");
-		button1.put("parent", "button");
-		button1.put("value", false);		
-		
-		JSONObject button2 = new JSONObject();
-		button2.put("name", "button2");
-		button2.put("parent", "button");
-		button2.put("value", true);
-		
-		//jsonStr = '{"theTeam":[{"teamId":"1","status":"pending"},{"teamId":"2","status":"member"},{"teamId":"3","status":"member"}]}';//
-		//var obj = JSON.parse(jsonStr);
-		//obj['theTeam'].push({"teamId":"4","status":"pending"});
-		//jsonStr = JSON.stringify(obj);
-		
-		
-		
- 
-		JSONArray data = new JSONArray();
-		data.add(button1);
-		data.add(button2);
-		data.add(button3);
-		//data.add("{'name':'Driver button 5'}");
-		//data.add("{'name':\'Driver button 4','parent':'Driver button 3','value':'false'}");
-		//data.add(etc.....);
+		builder.append("{\"ioinfo\":[\n");
+		builder.append("\t{\"name\":\"button1\",\"type\":\"button\",\"direction\":\"input\",\"port\":\"01\"},\n");		
+		builder.append("\t{\"name\":\"button2\",\"type\":\"button\",\"direction\":\"input\",\"port\":\"02\"},\n");	
+		builder.append("\t{\"name\":\"button3\",\"type\":\"button\",\"direction\":\"input\",\"port\":\"03\"}\n\t],\n"); // ], - on last entry!!!
 
-		
-		states.put("timestamp", System.currentTimeMillis());
-		states.put("values", data);
-		
-		state.add(states);
-		
-		//JSONObject stated = new JSONObject();
-		//stated.put("state", state);
-		//ioinfo.put("state", state);
-		
- 
-		// try-with-resources statement based on post comment below :)
-		try (FileWriter file = new FileWriter("/Users/EngiNERD1/Documents/file1.txt")) {
-			file.write(state.toJSONString());
-			System.out.println("Successfully Copied JSON state Object to File...");
-			System.out.println("\nJSON Object: " + state);
-		}catch (IOException e) {
-		    System.err.println("Caught IOException (state): " + e.getMessage());
-		}
-	
 	}
 	
-	public static void ioinfoJSON() {
+	
+	public static void testStateSB(StringBuilder stuff) {
 		
-		JSONObject port = new JSONObject();
+		builder = stuff;
 		
-		JSONObject button1 = new JSONObject();		//BUTTON1
-		button1.put("name", "button1");
-		button1.put("type", "button");
-		button1.put("direction", "Input");
-			port.remove("port");
-			port.put("channel", 1);
-		button1.put("port", port);
+
+		builder.append("\"state\":[\n");
+		
+		builder.append("\t{\"timestamp\":\"");
+		builder.append("1463365197324");
+		//builder.append(System.currentTimeMillis());
+		builder.append("\",\"values\":[\n");
+		builder.append("\t\t{\"name\":\"button1\",\"parent\":\"d-button1\",\"value\":\"");
+		builder.append(false);
+		builder.append("\"},\n");
+		builder.append("\t\t{\"name\":\"button2\",\"parent\":\"d-button2\",\"value\":\"false\"},\n");	
+		builder.append("\t\t{\"name\":\"button3\",\"parent\":\"d-button3\",\"value\":\"true\"}\n \t\t] }, \n"); // , - on last entry
+		
+		builder.append("\t{\"timestamp\":\"");
+		builder.append("1463365197334");
+		//builder.append(System.currentTimeMillis());
+		builder.append("\",\"values\":[\n");
+		builder.append("\t\t{\"name\":\"button1\",\"parent\":\"d-button1\",\"value\":\"true\"},\n");		
+		builder.append("\t\t{\"name\":\"button2\",\"parent\":\"d-button2\",\"value\":\"true\"},\n");	
+		builder.append("\t\t{\"name\":\"button3\",\"parent\":\"d-button3\",\"value\":\"true\"}\n \t\t] }, \n"); // , - on last entry
+			
+		builder.append("\t{\"timestamp\":\"");
+		builder.append("1463365197344");
+		//builder.append(System.currentTimeMillis());
+		builder.append("\",\"values\":[\n");
+		builder.append("\t\t{\"name\":\"button1\",\"parent\":\"d-button1\",\"value\":\"false\"},\n");		
+		builder.append("\t\t{\"name\":\"button2\",\"parent\":\"d-button2\",\"value\":\"false\"},\n");	
+		builder.append("\t\t{\"name\":\"button3\",\"parent\":\"d-button3\",\"value\":\"false\"}\n \t\t] } \n"); // , - on last entry
 		
 		
-		JSONObject button2 = new JSONObject();		//BUTTON2,  etc....
-		button2.put("name", "button2");
-		button2.put("type", "button");
-		button2.put("direction", "Input");
-			port.remove("port");
-			port.put("channel", 1);
-		button2.put("port", port);	
+		//PRINT FILE EVERY TIME?????
+		
+	}
+	
+	
+	public static void testFinalStateSB(StringBuilder stuff) {
+		
+		builder = stuff;
+		
+		builder.append("\t] \n}");			// close out JSON format
  
-		JSONArray info = new JSONArray();
-		info.add(button1);
-		info.add(button2);
-		logFile.put("ioinfo", info);
- 
-		// try-with-resources statement based on post comment below :)
-		try (FileWriter file = new FileWriter("/Users/EngiNERD1/Documents/file1.txt")) {
-			file.write(logFile.toJSONString());
-			System.out.println("Successfully Copied JSON ioinfo Object to File...");
-			System.out.println("\nJSON Object: " + logFile);
+		// try
+		try (FileWriter file = new FileWriter("/Users/Robin/Documents/file2.txt")) {
+			file.write(builder.toString());
+			System.out.println("Successfully Copied state String to File...");
+			System.out.println("builder string: \n" + builder);
 		}catch (IOException e) {
 		    System.err.println("Caught IOException (ioinfo): " + e.getMessage());
-		}
+		}	
+		
 	
+		
 	}
 	
-	public static void printJSON() {
-		
-		//stated = stated;
-		
-		//JSONObject fileOutput = new JSONObject();
-		//fileOutput.put("state", stated);
-		logFile.put("state", state);
-		
- 
-		// try-with-resources statement based on post comment below :)
-		try (FileWriter file = new FileWriter("/Users/EngiNERD1/Documents/file1.txt")) {
-			file.write(logFile.toJSONString());
-			System.out.println("Successfully Copied JSON LogFile Object to File...");
-			System.out.println("\nJSON Object: " + logFile);
-		}catch (IOException e) {
-		    System.err.println("Caught IOException (state): " + e.getMessage());
-		}
-	}
+	
+
 }
